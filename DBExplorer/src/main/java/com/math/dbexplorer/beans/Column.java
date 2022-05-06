1c2daee8e3c29b9f6a8c79e00fe89e4a044f5a37
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2022-05-06 13:52:26 -0300

    Columns types mapping

--- DBExplorer/src/main/java/com/math/dbexplorer/beans/Column.java
+++ DBExplorer/src/main/java/com/math/dbexplorer/beans/Column.java
@@ -47,16 +47,95 @@ public class Column {
     public void setIsPrimaryKey(boolean primaryKey) {
         this.primaryKey = primaryKey;
     }
-    
+
     public String getTreeName() {
         String diferentiator = this.isPrimaryKey() ? "(pk) " : "";
-        return diferentiator + this.getName() + " : " + Column.GetTypeName(this.getType());
-            
+        return diferentiator + this.getName() + " : " + Column.GetTypeName(this);
+
     }
-    
-    public static String GetTypeName(int type) {
-        return switch (type) {
-            default -> "Unknown";
+
+    public static String GetTypeName(Column col) {
+        return switch (col.getType()) {
+            case java.sql.Types.ARRAY ->
+                "ARRAY";
+            case java.sql.Types.BIGINT ->
+                "BIGINT";
+            case java.sql.Types.BINARY ->
+                "BINARY";
+            case java.sql.Types.BIT ->
+                "BIT";
+            case java.sql.Types.BLOB ->
+                "BLOB";
+            case java.sql.Types.BOOLEAN ->
+                "BOOLEAN";
+            case java.sql.Types.CHAR ->
+                "CHAR";
+            case java.sql.Types.CLOB ->
+                "CLOB";
+            case java.sql.Types.DATALINK ->
+                "DATALINK";
+            case java.sql.Types.DATE ->
+                "DATE";
+            case java.sql.Types.DECIMAL ->
+                "DECIMAL";
+            case java.sql.Types.DISTINCT ->
+                "DISTINCT";
+            case java.sql.Types.DOUBLE ->
+                "DOUBLE";
+            case java.sql.Types.FLOAT ->
+                "FLOAT";
+            case java.sql.Types.INTEGER ->
+                "INTEGER";
+            case java.sql.Types.JAVA_OBJECT ->
+                "JAVA_OBJECT";
+            case java.sql.Types.LONGNVARCHAR ->
+                "LONGNVARCHAR";
+            case java.sql.Types.LONGVARBINARY ->
+                "LONGVARBINARY";
+            case java.sql.Types.LONGVARCHAR ->
+                "LONGVARCHAR";
+            case java.sql.Types.NCHAR ->
+                "NCHAR";
+            case java.sql.Types.NCLOB ->
+                "NCLOB";
+            case java.sql.Types.NULL ->
+                "NULL";
+            case java.sql.Types.NUMERIC ->
+                "NUMERIC";
+            case java.sql.Types.NVARCHAR ->
+                "NVARCHAR";
+            case java.sql.Types.OTHER ->
+                "OTHER";
+            case java.sql.Types.REAL ->
+                "REAL";
+            case java.sql.Types.REF ->
+                "REF";
+            case java.sql.Types.REF_CURSOR ->
+                "REF_CURSOR";
+            case java.sql.Types.ROWID ->
+                "ROWID";
+            case java.sql.Types.SMALLINT ->
+                "SMALLINT";
+            case java.sql.Types.SQLXML ->
+                "SQLXML";
+            case java.sql.Types.STRUCT ->
+                "STRUCT";
+            case java.sql.Types.TIME ->
+                "TIME";
+            case java.sql.Types.TIME_WITH_TIMEZONE ->
+                "TIME_WITH_TIMEZONE";
+            case java.sql.Types.TIMESTAMP ->
+                "TIMESTAMP";
+            case java.sql.Types.TIMESTAMP_WITH_TIMEZONE ->
+                "TIMESTAMP_WITH_TIMEZONE";
+            case java.sql.Types.TINYINT ->
+                "TINYINT";
+            case java.sql.Types.VARBINARY ->
+                "VARBINARY";
+            case java.sql.Types.VARCHAR ->
+                "VARCHAR";
+            default ->
+                "Unknown";
         };
     }
 
